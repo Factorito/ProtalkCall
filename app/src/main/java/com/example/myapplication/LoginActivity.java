e4718a1278c714ab7505cfee2445a57b5a06ad53
Factorito <redbulue0509@gmail.com>
2024-12-03 15:28:35 +0900

    final code

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -5,36 +5,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText emailField, passwordField;
-    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // XML과 연결
-        emailField = findViewById(R.id.emailField);
-        passwordField = findViewById(R.id.passwordField);
-        loginButton = findViewById(R.id.loginButton);
 
-        // 로그인 버튼 클릭 리스너
         loginButton.setOnClickListener(v -> {
-            String email = emailField.getText().toString();
-            String password = passwordField.getText().toString();
 
-            if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(LoginActivity.this, "이메일과 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show();
-            } else {
-                // ComposeActivity로 이메일과 비밀번호 전달
-                Intent intent = new Intent(LoginActivity.this, ComposeActivity.class);
-                intent.putExtra("email", email);
-                intent.putExtra("password", password);
-                startActivity(intent);
-            }
+            startActivity(intent);
         });
     }
 }
